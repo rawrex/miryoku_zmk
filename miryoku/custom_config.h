f3fd3ec840e26b140d2462c43f7b316c8b2e6319
rawrex <rawrex@proton.me>
2025-05-12 21:51:13 +0700

    Corrected the current keyboard flag name

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -32,7 +32,7 @@ U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp 
 &kp DOT,           &kp N1,            &kp N2,            &kp N3,            &kp CARET,         &none,              &to U_NUM,           &to U_NAV,           &kp RALT,           &none,              \
 U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp EQUAL,         &none,              &none,              &none,              U_NP,               U_NP
 
-#ifdef MIRYOKU_KEYBOARD_MIRYOKU_XIAO
+#ifdef MIRYOKU_KEYBOARD_XIAO_BLE
 #define MIRYOKU_LAYERMAPPING_BASE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
@@ -43,17 +43,4 @@ U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp 
 &kp SEMI K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp COLON \
 &kp LBRC K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp RBRC \
                    K32  K33  K34       K35  K36  K37
-
-//#define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
-//#define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
-//#define MIRYOKU_KLUDGE_TOPROWCOMBOS_RIGHTINNERINDEX 7 8
-//#define MIRYOKU_KLUDGE_TOPROWCOMBOS_RIGHTPINKIE 8 9
-//
-//#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_LEFTPINKIE 26 27
-//#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_LEFTINNERINDEX 27 28
-//#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_RIGHTINNERINDEX 31 32
-//#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_RIGHTPINKIE 32 33
-//
-//#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 37 38
-//#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 39 40
 #endif
