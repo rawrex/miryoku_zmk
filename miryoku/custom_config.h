dcadd1b72987d278c4a9e8f9392d4a49225cdeca
rawrex <rawrex@proton.me>
2025-05-13 14:14:04 +0700

    Added the square brackets to the sym layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -43,4 +43,15 @@ U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp 
 &kp SEMI K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp COLON \
 &kp LBRC K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp RBRC \
                    K32  K33  K34       K35  K36  K37
+
+#define MIRYOKU_LAYERMAPPING_SYM( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&kp LT   K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp GT \
+&kp LBKT K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp COLON \
+&kp RBKT K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp RBRC \
+                   K32  K33  K34       K35  K36  K37
 #endif
