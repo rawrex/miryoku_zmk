7f03944f991169b4ca3a4349b3660d914a8a73d3
rawrex <rawrex@proton.me>
2025-12-31 10:58:36 +0700

    Revert "Reapply "Merge branch 'manna-harbour:master' into master""
    This reverts commit 9718bb4a1a0e0fc3b80621c54d83cad559356842.

--- miryoku/miryoku.h
+++ miryoku/miryoku.h
@@ -31,7 +31,11 @@
 
 #include "miryoku_shift_functions.h"
 
-#include "miryoku_mousekeys.h"
+#if defined (MIRYOKU_KLUDGE_MOUSEKEYSPR)
+  #include "miryoku_kludge_mousekeyspr.h"
+#else
+  #include "miryoku_mousekeys.h"
+#endif
 
 #if defined (MIRYOKU_KLUDGE_TAPDELAY)
   #include "miryoku_kludge_tapdelay.h"
